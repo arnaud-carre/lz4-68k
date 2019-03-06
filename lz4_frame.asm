92678f0172217a5dad24cc608386fb365f82f97d
arnaud <leonard.oxg@free.fr>
2019-03-06 20:57:44 +0100

    added LZ4 frame decoder

---
+++ lz4_frame.asm
@@ -0,0 +1,42 @@
+;---------------------------------------------------------
+;
+;	LZ4 Frame 68k depacker
+;	Written by Arnaud Carré ( @leonard_coder )
+;	https://github.com/arnaud-carre/lz4-68k
+;
+;	LZ4 technology by Yann Collet ( https://lz4.github.io/lz4/ )
+;	LZ4 frame description: https://github.com/lz4/lz4/blob/master/doc/lz4_Frame_format.md
+;
+;---------------------------------------------------------
+
+; input: a0.l : LZ4 frame addr ( 16bits aligned )
+; output: none
+;
+; Depack data produced by LZ4.exe command line:
+; 	lz4.exe -9 --no-frame-crc <input_file> <output_file>
+;
+
+lz4_frame_depack:
+		cmpi.l	#$04224d18,(a0)+	; LZ4 frame MagicNb
+		bne.s	lz4_frame_error
+
+		move.b	(a0),d0
+		andi.b	#%11001001,d0		; check version, no depacked size, and no DictID
+		cmpi.b	#%01000000,d0
+		bne.s	lz4_frame_error
+
+		movep.w	6(a0),d0			; read unaligned 32bits, little indian (LZ4 packed block size)
+		move.b	5(a0),d0
+		swap	d0
+		movep.w	4(a0),d0
+		move.b	3(a0),d0	
+		lea		7(a0),a0			; skip LZ4 block header + packed data size
+
+		bra.s	lz4_depack
+
+lz4_frame_error:
+		illegal
+		
+		include "lz4_smallest.asm"
+;		include "lz4_normal.asm"
+;		include "lz4_fastest.asm"
